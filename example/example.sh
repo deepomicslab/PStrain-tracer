8cffc51719586e665bbe875834050abc617699ec
yiqijiang17 <46921024+yiqijiang17@users.noreply.github.com>
2021-05-04 12:00:22 +0800

    Update example.sh

--- example/example.sh
+++ example/example.sh
@@ -10,6 +10,6 @@ perl ../src/PT-02-FMT_visual.pl  -WDR output -LS experimets_list.tsv -PS demo_PS
 perl ../src/PT-03-engraft.pl -WDR output -LS experimets_list.tsv -PS demo_PStrain_result/ -S 0.9 -m relative
 perl ../src/PT-03-engraft.pl -WDR output -LS experimets_list.tsv -PS demo_PStrain_result/ -S 0.9
 
-perl ../src/PT-04-heterzygousSNV_strainN.pl -WDR output  -PS demo_PStrain_result/ -S 0.9
+perl ../src/PT-04-heterzygousSNV_strainN.pl -WDR output -PS demo_PStrain_result/ -S 0.9
 perl ../src/PT-05-wilcox_testing.pl -WDR output -PS demo_PStrain_result/ -S 0.9 -LS group_info.tsv
 perl ../src/PT-06-detect.pl -WDR output -PS demo_PStrain_result/ -S 0.9 -LS strain.txt
